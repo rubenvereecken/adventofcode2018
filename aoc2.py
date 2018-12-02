698fee688fbc3ddc6457d499a22fe0aa4a91e891
Ruben Vereecken <rubenvereecken@gmail.com>
2018-12-02 11:10:50 +0000

    Cleaned up, better use of Trie.traverse

--- aoc2.py
+++ aoc2.py
@@ -23,33 +23,39 @@ def differ_barely(a, b):
     n_differ = sum(achar != bchar for achar, bchar in zip(a, b))
     return n_differ == 1
 
+
 def part_two():
     # need just two strings that differ by a single letter
     import pygtrie as trie
+    # Build a prefix tree out of the words
     t = trie.CharTrie()
     ids = list(read_lines())
+    # Populate trie, values are random (True)
     for word in ids:
         t[word] = True
-
-    result = [None]
 
     def trie_walk(path_conv, chars, children, whatsthis=None):
         path = path_conv(chars)
         # Got two subtrees here, where do they differ?
         words = list(t.keys(path))
         if len(words) == 2 and differ_barely(*words):
-            # Found them!
-            print(words)
-            result[0] = words
+            # Found them! Propagate them back up
             return words
         if len(words) < 1:
             return
         if len(words) > 2:
-            # we need to go deeper
-            list(children)
+            # we need to go deeper, this results in recursive call
+            result = list(children)
+            # Either there is one result, or none
+            # Filter out the result and bubble back up
+            result = list(filter(lambda x: x, result))
+            result = result[0] if result else None
+            return result
 
-    t.traverse(trie_walk)
-    for a, b in zip(*result[0]):
+    res = t.traverse(trie_walk)
+
+    # Now just print the common letters
+    for a, b in zip(*res):
         if a == b:
             print(a, end='')
     print()
